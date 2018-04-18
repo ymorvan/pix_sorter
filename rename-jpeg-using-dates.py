79c424f7f67575dc820eeadbaa3307e0ac586e51
Yannick Morvan <yannick.morvan.public@gmail.com>
2018-04-18 22:52:32 +0200

    Unified the exif parsing

--- rename-jpeg-using-dates.py
+++ rename-jpeg-using-dates.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
-# example https://gist.github.com/n3wtron/5858940
-from PIL import Image
-from PIL.ExifTags import TAGS
-import PIL.Image
+# author Yannick Morvan
+
+
 import datetime
 import argparse
 import os
@@ -11,16 +10,9 @@ from os.path import isfile, join
 import magic
 from shutil import copy2
 import hashlib
-
-
-def get_exif(fn):
-    ret = {}
-    i = PIL.Image.open(fn)
-    info = i._getexif()
-    for tag, value in info.items():
-        decoded = PIL.ExifTags.TAGS.get(tag, tag)
-        ret[decoded] = value
-    return ret
+import exiftool
+from datetime import datetime as DateTime
+import struct
 
 def md5sum(filename):
     md5 = hashlib.md5()
@@ -29,27 +21,67 @@ def md5sum(filename):
             md5.update(chunk)
     return md5.hexdigest()
 
+def exif_key(mime_type):
+    if 'jpeg' in mime_type:
+        return 'EXIF:CreateDate'
+    if 'quicktime' in mime_type or 'mp4' in mime_type:
+        return 'QuickTime:CreateDate'
+
+def name_from_exif(file_path, exif_mime_type_key):
+    filename, file_extension = os.path.splitext(file_path)
+    exif_Executable="/usr/bin/exiftool"
+
+    with exiftool.ExifTool() as et:
+        exif = et.get_metadata(file_path)
+#        print(exif)
+        if exif_mime_type_key in exif:
+            date_and_time = exif[exif_mime_type_key]
+            parsed_date = datetime.datetime.strptime(date_and_time, '%Y:%m:%d %H:%M:%S')
+            new_name = parsed_date.strftime("%Y-%m-%d-at-%Hh-%Mm-%S") + file_extension
+            year = parsed_date.strftime("%Y")
+            month = parsed_date.strftime("%m")
+            return new_name, year, month
+        else:
+            return md5sum(file_path) + file_extension, None, None
+
+
+
+def path_name(outdir, year, month, prev_fname, new_fname):
+    if year is None:
+        absolute_new_path_name = os.path.join(args.outdir, 'unsorted')
+        absolute_new_fname = os.path.join(absolute_new_path_name, new_fname)
+        return absolute_new_path_name, absolute_new_fname
+    else:
+        absolute_new_path_name = os.path.join(args.outdir, year, year + '-'+ month)
+        absolute_new_fname = os.path.join(absolute_new_path_name, new_fname)
+        return absolute_new_path_name, absolute_new_fname
+
+def copy_rename_file(outdir, year, month, prev_fname, new_fname):
+    absolute_new_path_name, absolute_new_fname = path_name(args.outdir, year, month, prev_fname, new_fname)
+    absolute_new_fname = absolute_new_fname.lower()
+    print('absolute_new_path_name ', absolute_new_path_name)
+    print('absolute_new_path_name ', absolute_new_fname)
+    # XXXXXXXXXXXX check path first, if not existYYYYYYYYYYYY then check file
+
+    if not os.path.exists(absolute_new_path_name):
+        os.makedirs(absolute_new_path_name, exist_ok=True)
+    if not os.path.exists(absolute_new_fname):
+        print("Copying ", prev_fname, ' to ', absolute_new_fname)
+        copy2(prev_fname, absolute_new_fname)
+    else:
+        print("File is already there, not overwritting ", absolute_new_fname)
+
 def main(args):
     unsorted_path = os.path.join(args.outdir, "unsorted")
     os.makedirs(unsorted_path, exist_ok=True)
     onlyfiles = [os.path.join(dp, f) for dp, dn, fn in os.walk(os.path.expanduser(args.indir)) for f in fn]
-    for file_path in onlyfiles:
-        ftype = magic.detect_from_filename(file_path).mime_type;
-        if 'jpeg' in ftype:
-            print (file_path)
-            exif = get_exif(file_path)
-            if 'DateTimeOriginal' in exif:
-                date_and_time = exif['DateTimeOriginal'];
-                parsed_date = datetime.datetime.strptime(date_and_time, '%Y:%m:%d %H:%M:%S')
-                new_name = parsed_date.strftime("%Y-%m-%d-at-%Hh-%Mm-%S") + ".jpg"
-                year = parsed_date.strftime("%Y")
-                month = parsed_date.strftime("%m")
-                target_path = os.path.join(args.outdir, year, month)
-                target_name = os.path.join(args.outdir, year, month, new_name)
-                os.makedirs(target_path, exist_ok=True)
-                copy2(file_path, target_name)
-            else:
-                copy2(file_path, os.path.join(unsorted_path, md5sum(file_path)) + ".jpg" )
+    for current_fname in onlyfiles:
+        mime_type = magic.detect_from_filename(current_fname).mime_type
+        if 'jpeg' in mime_type or 'quicktime' in mime_type or 'mp4' in mime_type:
+            exif_mime_type_key = exif_key(mime_type);
+            new_fname, year, month  = name_from_exif(current_fname, exif_mime_type_key)
+            copy_rename_file(args.outdir, year, month, current_fname, new_fname)
+
 
 
 if __name__=='__main__':
